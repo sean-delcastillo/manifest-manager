0ce9be4a0b713d9c70e35f997296d46a3b3be5d1
Sean Del Castillo <seanbdcastillo@gmail.com>
2024-06-08 17:50:37 -0700

    styling

--- widgets/manager_menu.py
+++ widgets/manager_menu.py
@@ -1,5 +1,3 @@
-from typing import Optional
-from textual import on
 from textual.reactive import reactive
 from textual.widgets import Static, Label, Button
 from textual.app import ComposeResult
@@ -14,17 +12,13 @@ class ManagerMenu(Static):
     count_local_files_only: reactive[int] = reactive(0)
 
     DEFAULT_CSS = """
-    #manifestmenu-loaded-warning {
-        background: orange 50%;
-        width: auto;
-        margin-left: 1;
-        margin-right: 1;
-        padding-left: 1;
-        padding-right: 1;
+    ManagerMenu {
+        margin-top: 1;
+        margin-bottom: 1;
     }
 
-    .label_highlight {
-        color: red 90%;
+    #manifestmenu-loaded-warning {
+        width: auto;
     }
 
     Vertical {
@@ -51,13 +45,13 @@ class ManagerMenu(Static):
                     yield Label(
                         "Please Load a Remote and Local Manifest",
                         id="manifestmenu-loaded-warning",
+                        classes="highlight-warning",
                     )
                 with Center():
                     yield Label(
-                        id="local-manifest-outdated-warning",
-                        classes="label_highlight",
+                        id="local-manifest-outdated-warning", classes="highlight-error"
                     )
-                    yield Label(id="all-good-warning")
+                    yield Label(id="all-good-warning", classes="highlight-ok")
                 with Center():
                     yield Button("Sync Local", id="sync-button")
 
@@ -75,6 +69,10 @@ class ManagerMenu(Static):
         warning_label = self.query_one("#manifestmenu-loaded-warning", Label)
         if new_is_local_manifest_loaded and self.is_remote_manifest_loaded:
             warning_label.display = False
+            if self.count_remote_files_only <= 0:
+                all_good = self.query_one("#all-good-warning", Label)
+                all_good.display = True
+                all_good.update("Local Manifest is Up-To-Date with Remote")
         else:
             warning_label.display = True
 
@@ -82,13 +80,16 @@ class ManagerMenu(Static):
         warning_label = self.query_one("#manifestmenu-loaded-warning", Label)
         if new_is_remote_manifest_loaded and self.is_local_manifest_loaded:
             warning_label.display = False
+            if self.count_remote_files_only <= 0:
+                all_good = self.query_one("#all-good-warning", Label)
+                all_good.display = True
+                all_good.update("Local Manifest is Up-To-Date with Remote")
         else:
             warning_label.display = True
 
     def watch_count_remote_files_only(self, new_count: int):
         outdated_warning = self.query_one("#local-manifest-outdated-warning", Label)
         sync_button = self.query_one("#sync-button", Button)
-        all_good = self.query_one("#all-good-warning", Label)
         if new_count > 0:
             outdated_warning.update(
                 f"Local Manifest is {new_count} Files Behind Remote Manifest"
@@ -98,7 +99,3 @@ class ManagerMenu(Static):
         else:
             outdated_warning.display = False
             sync_button.display = False
-
-            if self.is_local_manifest_loaded and self.is_remote_manifest_loaded:
-                all_good.display = True
-                all_good.update("Local Manifest is Up-To-Date with Remote")
